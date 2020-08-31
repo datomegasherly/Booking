d0bf6856b986201a92dd7c2853fc857f67163891
datomegasherly <dat.omega.sherly@gmail.com>
2020-08-31 13:43:36 +0430

    add css and fonts , add booking template , some fixes in webpack config . some modules add to package.json

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,18 @@
 import React, { Component } from 'react';
-import './index.css';
+import './css/open-iconic-bootstrap.min.css';
+import './css/animate.css';
+import './css/owl.carousel.min.css';
+import './css/owl.theme.default.min.css';
+import './css/magnific-popup.css';
+import './css/aos.css';
+import './css/ionicons.min.css';
+import './css/bootstrap-datepicker.css';
+import './css/jquery.timepicker.css';
+import './css/flaticon.css';
+import './css/icomoon.css';
+import './css/style.css';
+
+//navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light
 
 class App extends Component {
     render() {
